f3886886e1b6a6ef331df556e509412882887ac9
Руслан Галяудинов <rus4855@yandex.ru>
2025-12-11 20:51:09 +0300

    Создал класс Product

---
+++ src/class_product.py
@@ -0,0 +1,13 @@
+class Product:
+    """Класс для обработки информации о продуктах."""
+
+    name: str        # Название продукта
+    description: str # Описание характеристик продукта
+    price: float     # Цена продукта
+    quantity: int    # Количество продукта в наличии
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
